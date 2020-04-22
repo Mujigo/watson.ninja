8bfd21d7ae4a44ba46e05d75a595d5c2159471ee
Stephen Watson <mujigo@gmail.com>
2020-04-22 11:56:46 -0500

    Added projects page and reworked index.vue

---
+++ pages/projects.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="(project, index) in projects" :key="index" align-self="center">
+        <v-card shaped max-width="300">
+          <v-img :src="project.img" contain width="300" />
+          <v-card-title>{{ project.name }}</v-card-title>
+          <v-card-text>{{ project.description }}</v-card-text>
+          <v-divider />
+          <v-card-text>
+            <v-btn :href="project.link" target="_blank">
+              Check It Out!
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      projects: [
+        {
+          name: 'Save the Frogs',
+          description: 'Save the Frogs is one of the projects I completed for the Udacity Front End Development course.',
+          img: 'images/project-images/save-the-frogs.png',
+          link: 'https://savethefrogs.watson.ninja'
+        },
+        {
+          name: 'Memory Game',
+          description: 'A Memory Game that was one of the project for the Udacity Front End Development course.',
+          img: 'images/project-images/memory-game.png',
+          link: 'https://memory.watson.ninja/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
